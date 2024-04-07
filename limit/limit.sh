d3253a69d97e54d52e530aa5a3ff5e21ee86eb5b
xssh99 <jb.ssh99@gmail.com>
2024-04-07 23:41:57 +0800

    jbc

---
+++ limit/limit.sh
@@ -0,0 +1,21 @@
+REPO="https://lite.scvps.biz.id/"
+wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPO}limit/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}limit/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}limit/trojan" >/dev/null 2>&1
+chmod +x /etc/xray/limit.vmess
+chmod +x /etc/xray/limit.vless
+chmod +x /etc/xray/limit.trojan
+systemctl daemon-reload
+systemctl enable --now limitvmess
+systemctl enable --now limitvless
+systemctl enable --now limittrojan
+# systemctl start limitvmess
+# systemctl start limitvless
+# systemctl start limittrojan
+# systemctl start limitshadowsocks
+# systemctl restart limitvmess
+# systemctl restart limitvless
+# systemctl restart limittrojan
